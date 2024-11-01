babd8c5bc80a7ddcd347ba07d83c6ff96b75da07
Babatunde-Segun <officiallyvincify@gmail.com>
2024-11-01 09:27:18 +0100

    non blocking code with readFile

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const { error } = require("console");
 const fs = require("fs");
 
-const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textInput);
+// Blocking, synchronous way
 
-const textOutput = `This is what we know about the avocoado: ${textInput}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOutput);
+// const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textInput);
 
-console.log("File written");
+// const textOutput = `This is what we know about the avocoado: ${textInput}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOutput);
+
+// console.log("File written");
+
+// Non blocking
+fs.readFile("./txt/start.txt", "utf-8", (error, data) => {
+  console.log(data);
+
+  fs.readFile(`./txt/${data}.txt`, "utf-8", (error, data4) => {
+    console.log(data4);
+    fs.readFile("./txt/append.txt", "utf-8", (error, data2) => {
+      console.log(data2);
+
+      fs.writeFile("./txt/final.txt", `${data}\n${data2}`, "utf-8", (err) => {
+        console.log("Your file has been written😄");
+      });
+    });
+  });
+});
+
+console.log("Will read file");
